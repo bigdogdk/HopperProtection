af0a8160845aeba481341644c1f6f087e72c990b
bigdogdk <koberczky@gmail.com>
2014-01-29 13:30:48 -0600

    added LWC support

--- deadboltHopperProtection/src/com/gmail/bigdogdak/deadboltHopperProtection/DeadboltHopperProtection.java
+++ deadboltHopperProtection/src/com/gmail/bigdogdak/deadboltHopperProtection/DeadboltHopperProtection.java
@@ -2,9 +2,11 @@ package com.gmail.bigdogdak.deadboltHopperProtection;
 
 import com.daemitus.deadbolt.Deadbolt;
 import com.daemitus.deadbolt.Deadbolted;
+
+import com.griefcraft.lwc.LWC;
+
 import java.util.logging.Logger;
 import org.bukkit.Bukkit;
-import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.Chest;
 import org.bukkit.block.DoubleChest;
@@ -12,9 +14,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryMoveItemEvent;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
-import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class DeadboltHopperProtection
@@ -24,6 +24,7 @@ public class DeadboltHopperProtection
   public static final Logger log = Logger.getLogger("Minecraft");
   Block block;
   DoubleChest dc;
+  LWC lwc;
   
   public void load(Deadbolt plugin) {}
   
@@ -40,6 +41,9 @@ public class DeadboltHopperProtection
         if (d.isProtected()) {
           event.setCancelled(true);
         }
+        else if(LWC.getInstance().findProtection(chest.getWorld().getBlockAt(chest.getLocation())) != null){
+        	event.setCancelled(true);
+        }
       }
     }
     else if (event.getSource().getSize() == 54)
@@ -48,9 +52,12 @@ public class DeadboltHopperProtection
       if ((holder != null) && ((holder instanceof DoubleChest)))
       {
         DoubleChest chest = (DoubleChest)holder;
-        Deadbolted d = Deadbolt.get(chest.getWorld().getBlockAt(chest.getLocation()));
-        if (d.isProtected()) {
+        Deadbolted dp = Deadbolt.get(chest.getWorld().getBlockAt(chest.getLocation()));
+        if (dp.isProtected()) {
           event.setCancelled(true);
+        }
+        else if(LWC.getInstance().findProtection(chest.getWorld().getBlockAt(chest.getLocation())) != null){
+        	event.setCancelled(true);
         }
       }
     }
